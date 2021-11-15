5acbf4ae6e1abab443a0467e36b883a38e70489a
ABCpril <909046646@qq.com>
2021-11-15 10:06:10 +0800

    支持JSON格式登录

--- multi-filter-chain/src/main/java/com/sspath/multifilterchain/config/LoginFilter.java
+++ multi-filter-chain/src/main/java/com/sspath/multifilterchain/config/LoginFilter.java
@@ -1,6 +1,11 @@
 package com.sspath.multifilterchain.config;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -8,6 +13,9 @@ import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @FileName: LoginFilter.java
@@ -21,8 +29,34 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         if (!request.getMethod().equals("POST")) {
             throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
         }
-        String kaptcha = request.getParameter("kaptcha");
+
         String sessionKaptcha = (String) request.getSession().getAttribute("kaptcha");
+
+        if (request.getContentType().equalsIgnoreCase(MediaType.APPLICATION_JSON_VALUE) ||
+                request.getContentType().equalsIgnoreCase(MediaType.APPLICATION_JSON_UTF8_VALUE)) {
+            Map<String, String> userInfo = new HashMap<>();
+            try {
+                userInfo = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+                String username = userInfo.get(getUsernameParameter());
+                String password = userInfo.get(getPasswordParameter());
+                String kaptcha = userInfo.get("kaptcha");
+                if (StringUtils.hasText(kaptcha) && StringUtils.hasText(sessionKaptcha) && kaptcha.equalsIgnoreCase(sessionKaptcha)) {
+                    UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+                            username, password);
+                    setDetails(request, authRequest);
+                    return this.getAuthenticationManager().authenticate(authRequest);
+                }
+            } catch (JsonMappingException e) {
+                e.printStackTrace();
+            } catch (JsonParseException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String kaptcha = request.getParameter("kaptcha");
+
         if (StringUtils.hasText(kaptcha) && StringUtils.hasText(sessionKaptcha) && kaptcha.equalsIgnoreCase(sessionKaptcha)) {
             return super.attemptAuthentication(request, response);
         }
